eb260370465be419955cbf7f69569e0f73d39b84
Masato setoyama <setoyama60jp@gmail.com>
2013-08-03 17:40:31 +0900

    modified recommend logic

--- r_scripts/recommend_gem.r
+++ r_scripts/recommend_gem.r
@@ -2,11 +2,16 @@ library(arules)
 
 load("./gemRecoModel.RData")
 
+#存在する条件部だけにフィルタを書けるメソッド
+inner.filter.gemlist <- function(gem.list) {
+  intersect(itemLabels(lhs(model.apriori)), gem.list)
+}
+
 #ルールの右側だけを取り出す関数
 
 extract.rhs <- function(str) {
   result <- unlist(strsplit(str, "=>"))
-  tail(result, n=1)
+  result[seq(2, length(result), by=2)]
 }
 
 recommend.gem <- function(gem.list) {
@@ -19,17 +24,17 @@ recommend.gem <- function(gem.list) {
 
 inner.recommend.gem <- function(gem.list) {
   
-  input.list <- gem.list
+  input.list <- inner.filter.gemlist(gem.list)
   result.list <- list()
   
   #ワークスペースに保存されているmodel.aprioriを使用する
-  subset.result <- subset(model.apriori, subset = lhs %in% input.list[1] & lift > 2)
+  subset.result <- subset(model.apriori, subset = lhs %in% input.list & lift > 2)
 
-  #結果の上位1件をデータフレーム形式に変換
-  model.df <- as(head(sort(subset.result, by="confidence"), n = 1), "data.frame")
+  #結果をデータフレーム形式に変換
+  model.df <- as(sort(subset.result, by="confidence"), "data.frame")
   
-  #上位1件を結果に追加
-  result.list <- c(result.list, extract.rhs(as.character(model.df$rules[1])))
+  #結果に追加
+  result.list <- c(result.list, extract.rhs(as.character(model.df$rules)))
   
   return (result.list)
 }
